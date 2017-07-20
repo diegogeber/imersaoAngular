b03f0e86209e749f298acd06305860c853ae991c
Victor Coutinho <victormelo@id.uff.br>
2017-07-19 22:02:20 -0300

    Victor Coutinho REFS #02.*, 03.*: Criação de classes, serviços e componentes para usuario e tarefa.

---
+++ src/app/services/tarefa.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import { Headers, Http } from "@angular/http";
+import 'rxjs/add/operator/toPromise';
+
+import { Tarefa } from '../shared/tarefa';
+
+@Injectable()
+export class TarefaService {
+  private urlBase:string = "localhost:8080/todo";
+
+  private headers = new Headers({'content-type':'application/json'});
+
+  
+  constructor(private http:Http) { }
+
+  getTarefas():Promise<Tarefa[]>{
+    return this.http.get(this.urlBase)
+      .toPromise()
+      .then(Response=>Response.json() as Tarefa[])
+      .catch(this.handleError);
+
+  }
+
+  getTarefasCriador(id:number):Promise<Tarefa[]>{
+    return this.http.get(this.urlBase+"/criador/"+id)
+      .toPromise()
+      .then(Response=>Response.json() as Tarefa[])
+      .catch(this.handleError);
+
+  }
+  
+  getTarefa(id:number):Promise<Tarefa>{
+    return this.http.get(this.urlBase+"/"+id)
+      .toPromise()
+      .then(Response=>Response.json() as Tarefa)
+      .catch(this.handleError);
+
+  }
+  
+  update(tarefa:Tarefa):Promise<Tarefa>{
+    return this.http.put(this.urlBase+"/"+tarefa.id, tarefa)
+    .toPromise()
+    .then(()=>tarefa)
+    .catch(this.handleError);
+  }
+
+  create(tarefa:Tarefa):Promise<string>{
+    return this.http.post(this.urlBase, tarefa)
+    .toPromise()
+    .then(Response=>Response.json)
+    .catch(this.handleError);
+  }
+
+  delete(id:number):Promise<string>{
+    return this.http.delete(this.urlBase+"/"+id)
+    .toPromise()
+    .then(Response=>Response.json)
+    .catch(this.handleError);
+  }
+
+
+private handleError(error: any): Promise<any> {
+  console.error('Ocorreu o seguinte erro', error); // Para Aparecer no console
+  return Promise.reject(error.message || error);
+}
+}
---
+++ src/app/shared/tarefa.ts
@@ -0,0 +1,8 @@
+export class Tarefa {
+    id:number;
+    titulo:string;
+    descricao:string;
+    dataCriacao:string;
+    dataFim:string;
+    criador:number;
+}
